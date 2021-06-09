76c33bcffd2710fb8773210b8b86922d4083b829
albertoclarit <albertoclarit@gmail.com>
2021-06-09 18:18:15 +0800

    After the demo

---
+++ src/main/java/ch/keybridge/faces/jsf/bs5/ButtonType.java
@@ -0,0 +1,9 @@
+package ch.keybridge.faces.jsf.bs5;
+
+public enum ButtonType {
+    PRIMARY,
+    SECONDARY,
+    SUCCESS,
+    WARNING,
+
+}
---
+++ src/main/java/ch/keybridge/faces/jsf/bs5/NavItemType.java
@@ -0,0 +1,7 @@
+package ch.keybridge.faces.jsf.bs5;
+
+public enum NavItemType {
+    BRAND,
+    LINK,
+    CUSTOM
+}
